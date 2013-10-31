85ca7b1b1733f89a8cb725fb1c93e39bbfd84753
ThanhNguyen <johnsmeeth0210@gmail.com>
2013-10-31 13:21:32 +0700

    Add more some feature like next, previous and support option loop

--- scripts/angularSlideShow.js
+++ scripts/angularSlideShow.js
@@ -1,75 +1,51 @@
 var app = angular.module('App', []);
 
 app.factory('images', function(){
-	return [
-		{id: 0, name: 'Hà Nội', path: 'images/1.jpg'},
-		{id: 1, name: 'HCM', path: 'images/2.jpg'},
-		{id: 2, name: 'Huế', path: 'images/3.jpg'},
-		{id: 3, name: 'Hải Phòng', path: 'images/4.jpg'},
-		{id: 4, name: 'Cần Thơ', path: 'images/5.jpg'},
-		{id: 5, name: 'Đà Nẵng', path: 'images/6.jpg'},
-	];
+    return [
+        {id: 0, name: 'Hà Nội', path: 'images/1.jpg'},
+        {id: 1, name: 'HCM', path: 'images/2.jpg'},
+        {id: 2, name: 'Huế', path: 'images/3.jpg'},
+        {id: 3, name: 'Hải Phòng', path: 'images/4.jpg'},
+        {id: 4, name: 'Cần Thơ', path: 'images/5.jpg'},
+        {id: 5, name: 'Đà Nẵng', path: 'images/6.jpg'},
+    ];
 });
 
 //controller
 app.controller('slideCtrl', function($scope, images, $timeout){
 
-	//Define and init model
-	$scope.items = images; 
-	$scope.img = $scope.items[0].path;
-	$scope.isActive = false;
-	
+    //Define and init model
+    $scope.items = images;
+    //$scope.img = $scope.items[0].path;
+    $scope.img = {id: $scope.items[0].id, path: $scope.items[0].path};
 
-	//Declare method : Hover mouse change image
-	$scope.changeImage = function(index){
-		$scope.img = $scope.items[index].path;
-		$scope.isCurrentActive = true;
+    //Declare method : Hover mouse change image
+    $scope.changeImage = function(index){
+        $scope.img.id = index;
+        $scope.img.path = $scope.items[index].path;
+    }
 
-	}
+    var idImg = 0;
+    // init Auto change images slide
+    $scope.autoChange = function(){
+        if(idImg == 6){idImg = 0};
+        $scope.img.id = idImg;
+        $scope.img.path = $scope.items[idImg].path;
+        idImg++;
+        var myTimer = $timeout($scope.autoChange, 2000);
+    }
 
-	/*Defined Active Class Hover || Auto Change*/
-	$scope.active = "";
+    $scope.next = function(currentIndex){
+        if(currentIndex == 5){currentIndex = -1};
+        $scope.img.id = currentIndex + 1;
+        $scope.img.path = $scope.items[currentIndex + 1].path;
+        console.log('img.id = ' + $scope.img.id + ', img.path = ' + $scope.img.path);
+    }
 
-	/*Defined id images*/
-	var idImg = 0;
-	var active = 1;
-	$scope.getId = function(index){
-		console.log(index);
-	}
-	// init Auto change images slide
-	$scope.autoChange = function(){
-
-		if(idImg == 5){idImg = 0};
-
-		$scope.img = $scope.items[idImg].path;
-		if(idImg == $scope.getId()){
-			$scope.getId(idImg) = "ActiveClass";
-		}
-		if($scope.getId())
-
-		idImg++; //3
-
-		myTimer = $timeout($scope.autoChange, 2000);
-		active = 0;
-	}
-
-
-
-
+    $scope.previous = function(currentIndex){
+        if(currentIndex == 0){currentIndex = 6};
+        $scope.img.id = currentIndex - 1;
+        $scope.img.path = $scope.items[currentIndex - 1].path;
+        console.log('img.id = ' + $scope.img.id + ', img.path = ' + $scope.img.path);
+    }
 });
-
-
-
-			/*-- Auto play with badway (traditional of javascript) --
-	$scope.autoChangeDanToc = function(){
-		var index = 0;
-		//check index when at final
-		var timer = setInterval(function(){
-			if(index == 5){index = 0};
-			$scope.img = $scope.items[index].path;
-			$scope.$apply();
-			console.log('index = ' + index);
-			index++;
-		}, 2000);
-	}
-		/*-- Auto play with good way by angular --*/
